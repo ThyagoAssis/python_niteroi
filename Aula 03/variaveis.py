c18c15eec8ec20f6351b4cfdb247d397330ea2a6
thyagoassis <thyagoalmeidaprof@gmail.com>
2024-06-10 15:19:01 -0300

    Aula 01

--- Aula 03/variaveis.py
+++ Aula 03/variaveis.py
@@ -5,15 +5,31 @@ Principais tipos de variáveis
     >int - Valor inteiro
     >float - Valor fracionário
     >boolean - Verdadero ou falso
+    >complex - parte real e uma parte imaginária. forma a + bi,
+                onde a e b são números reais e i é a unidade imaginária
+                Por exemplo, 2 + 3j, -1 + 2j são números complexos em Python.
+    >list - Lista de itens ex: [1,2,3]
+    >Tuplas (tuple) -  semelhantes às listas, mas são imutáveis, ou seja, não podem ser modificadas após a sua criação
+                        ex: (1, 2, 3)
+
+
+
+
 '''
 #A declaração de variavel no python utiliza snake_case
 curso = "Senac"
 numero = "20"
 numero_dois = 20
+complex = 2 + 3j
+list = [1,2,3]
+tupla = (1,2,3)
 
+print(tupla)
 #A função type verifica o tipo da variável
 print("O tipo é: ", type(numero))
-print("O tipo é: ", type(numero_dois))
+print("O tipo é: ", type(list))
+
+
 
 
 
